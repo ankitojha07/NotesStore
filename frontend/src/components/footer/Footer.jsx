9cc6d2b2b5bec3c913e4307b146fe71f41d39688
Ankit Ojha <ankitojha1409@gmail.com>
2023-03-15 02:55:32 +0530

    NotesStore

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <h1 className="flex justify-center bg-pink-800 w-full py-4 text-white fixed bottom-0">
-      Copyright &copy; NoteZipper <span className="ml-1"> </span>
+      Copyright &copy; NotesStore <span className="ml-1"> </span>
     </h1>
   );
 };
